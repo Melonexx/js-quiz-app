86417e8569296943d71ecce79d02dc33f625a853
Eileen Ilsemann <eileenilsemanni@gmail.com>
2023-12-14 09:47:53 +0100

    Toggle hidden class + Change text content of button

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-// phew… not a lot going on here. Please add some code!
-const header = document.querySelector('[data-js="bookmark"]');
+const bookmark = document.querySelector('[data-js="bookmark"]');
 
-header.addEventListener("click", () => {
-  header.classList.toggle("bookmark--active");
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
+
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const card = document.querySelector('[data-js="card-answer"]');
+
+answerButton.addEventListener("click", () => {
+  card.classList.toggle("hidden");
+
+  if (answerButton.textContent === "Hide Answer") {
+    answerButton.textContent = "Show Answer";
+  } else {
+    answerButton.textContent = "Hide Answer";
+  }
 });
